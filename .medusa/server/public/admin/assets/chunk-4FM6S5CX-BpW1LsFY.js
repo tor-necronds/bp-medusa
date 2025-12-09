8ce8dd9df0aae8f8c911b0da9552e2fa6b72d9dd
Tor (Front-end) <nanthawat.wangpoomyai@necronds.com>
2025-12-09 10:53:17 +0700

    Remove deprecated files and configurations from the .medusa folder
    - Deleted entry.jsx, index.css, and index.html to streamline the client setup.
    - Removed instrumentation.js, medusa-config.js, and other server-related files to clean up the server structure.
    - Eliminated unused assets and scripts to enhance project maintainability and reduce clutter.

--- .medusa/server/public/admin/assets/chunk-4FM6S5CX-BpW1LsFY.js
+++
@@ -1,1 +0,0 @@
-var e=new Intl.NumberFormat([],{style:"percent",minimumFractionDigits:2,maximumFractionDigits:4}),m=(r,a=!1)=>{let t=r||0;return a||(t=t/100),e.format(t)};export{m as f};
--- .medusa/server/src/api/admin/products/[id]/brand/validators.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PostAdminSetProductBrand = void 0;
-const zod_1 = require("zod");
-exports.PostAdminSetProductBrand = zod_1.z.object({
-    brand_id: zod_1.z.string().nullable().optional(),
-});
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmFsaWRhdG9ycy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uLy4uLy4uLy4uLy4uL3NyYy9hcGkvYWRtaW4vcHJvZHVjdHMvW2lkXS9icmFuZC92YWxpZGF0b3JzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLDZCQUF3QjtBQUVYLFFBQUEsd0JBQXdCLEdBQUcsT0FBQyxDQUFDLE1BQU0sQ0FBQztJQUMvQyxRQUFRLEVBQUUsT0FBQyxDQUFDLE1BQU0sRUFBRSxDQUFDLFFBQVEsRUFBRSxDQUFDLFFBQVEsRUFBRTtDQUMzQyxDQUFDLENBQUMifQ==
