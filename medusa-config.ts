74c012ded0298674a143af1e4c2974452760559a
Tor (Front-end) <nanthawat.wangpoomyai@necronds.com>
2025-12-11 10:49:56 +0700

    Refactor Medusa configuration and scripts for simplicity
    - Removed redisUrl from medusa-config for cleaner configuration.
    - Simplified build and start scripts by removing unnecessary NODE_OPTIONS and environment settings.

--- medusa-config.ts
+++ medusa-config.ts
@@ -12,7 +12,6 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
-    redisUrl: process.env.REDIS_URL,
   },
   modules: [
     {
